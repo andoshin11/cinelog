b69070ddd263d082db07be7171f3dba7f0962944
Andy ver 3.5 <shin.ando@meetus.jp>
2017-02-13 23:11:04 +0900

    :+1: Update Posts Index Design

--- app/assets/javascripts/posts/posts.vue
+++ app/assets/javascripts/posts/posts.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="posts row">
     <a :href="'/posts/' + post.id" class="post" v-for="post in posts">
-      <div class="post__thumbnail" :style="{ backgroundImage: 'url(' + thumbnailUrl + ')' }"></div>
+      <div class="post__thumbnail" :style="{ backgroundImage: 'url(' + 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + post.movie.posterPath + ')' }"></div>
       <div class="post__info">
-        <div class="post__date">{{ post.updatedAt }}</div>
-        <div class="post__title">{{ title }}</div>
-        <div class="post__movie">{{ title }}</div>
+        <div class="post__date">{{ post.updatedAt | neatDate }}</div>
+        <div class="post__title">{{ post.title }}</div>
+        <div class="post__movie">{{ post.movie.title }}</div>
         <div class="post__comment">{{ post.comment }}</div>
       </div>
     </a>
--- app/views/api/posts/index.json.jbuilder
+++ app/views/api/posts/index.json.jbuilder
@@ -3,6 +3,19 @@ json.posts do
     json.(
       post,
       :id, :title, :comment, :updated_at,
-    )
+    )  
+    json.movie do    
+      if post.movie_id.present?
+        json.(
+          Movie.find(post.movie_id),
+          :id, :title, :poster_path, :mvdb_id, :backdrop_path,
+          )
+      else
+        json.(
+          Movie.new,
+          :id, :title, :poster_path, :mvdb_id, :backdrop_path,
+        )
+      end
+    end
   end
 end
